21ac85e7ce9c5f82683a62aff75c1be4b1993ca4
Mindaugas Celiesius <m.celiesius@yandex.ru>
2023-09-05 15:28:26 +0300

    Updated ctwm config

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # Install packages after installing base Debian with no GUI
 
 # xorg display server installation and wm
-sudo apt install -y xorg xterm rxvt-unicode
+sudo apt install -y xorg xterm terminator mwm
 
 #WM
-sudo apt install -y suckless-tools xautolock micro screen
+sudo apt install -y xscreensaver
 
 #Other stuff
-sudo apt install -y dunst libnotify-bin feh
+sudo apt install -y dunst libnotify-bin imagemagick
 
 # File Manager (eg. pcmanfm,krusader)
 sudo apt install -y w3m mc 
@@ -21,7 +21,7 @@ sudo apt install -y udisks2
 sudo apt install -y neofetch htop lm-sensors smartmontools
 
 # Various utilities
-sudo apt install -y imagemagick  gnome-icon-theme rar unrar p7zip-full p7zip-rar zip unzip lxappearance
+sudo apt install -y gnome-icon-theme rar unrar p7zip-full p7zip-rar zip unzip lxappearance
 
 
 # Browser and Mail client Installation (eg. chromium)
@@ -34,16 +34,22 @@ sudo apt install -y zathura zathura-djvu
 sudo apt install -y fonts-font-awesome fonts-ubuntu fonts-liberation2 fonts-liberation xfonts-terminus fonts-jetbrains-mono
 
 #Music and video packages
-sudo apt install -y mpv alsa-utils moc
+sudo apt install -y mpv alsa-utils moc pipewire-audio pavucontrol
 
 #Torrents
-sudo apt install -y qbittorrent transmission-gtk
+sudo apt install -y qbittorrent transmission-gtk gufw lxpolkit
 
 #Install Office packages
 sudo apt install -y libreoffice-writer libreoffice-gtk3  
 
-#Plan9
-sudo apt install -y build-essential libx11-dev libxext-dev libxt-dev xorg-dev
+#Emwm
+sudo apt install -y build-essential libx11-dev libxt-dev libxext-dev libxrandr-dev libxinerama-dev libmotif-dev libjpeg-dev libpng-dev libtiff-dev
+
+#Headers
+sudo apt install -y linux-headers-amd64
+
+#Nvidia
+sudo apt install -y nvidia-tesla-470-driver
 
 sudo apt autoremove
 
